f9654e62cc74435cffac9c9743ad2de46d2b8830
Lee-Ha-Eun-00 <lhe543@naver.com>
2020-10-04 19:47:50 +0900

    # WARNING: head commit changed in the meantime
    Quize5

---
+++ Homework/src/Homework.java
@@ -0,0 +1,33 @@
+Must declare a named package cecause this compilation unit is associated to the named module 'Homework'
+import java.util.Scanner;
+
+public class Homework {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sanner scanner =new Sanner(System.in);
+		
+		System.out.print("첫번째 원의 중심과 반지름 입력 >> ");
+		int circle1X = scanner.nextInt();
+		int circle1Y = scanner.nextInt();
+		int circle1Radius=scanner.nextInt();
+		
+		System.out.print("첫번째 원의 중심과 반지름 입력 >> ");
+		int circle2X = scanner.nextInt();
+		int circle2Y = scanner.nextInt();
+		int circle2Radius=scanner.nextInt();
+		
+		int distanceSquare = (circle1X-circle2X)*(circle1X-circle2X)+(circle1Y-circle2Y)*(circle1Y-circle2Y);
+		int sumRadius = circle1Radius + circle2Radius;
+		if(ditanceSquare<sumRadius)
+		{
+			System.out.println("두 원은 서로 겹친다.");
+		}
+		else
+		{
+			System.out.println("두 원은 서로 겹치지 않는다.");
+		}
+		
+		scanner.close();
+	}
+
+}
---
+++ Task2/src/Task2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner =new Scanner(System.in);
+		
+		System.out.print("첫번째 원의 중심과 반지름 입력 >> ");
+		int circle1X = scanner.nextInt();
+		int circle1Y = scanner.nextInt();
+		int circle1Radius=scanner.nextInt();
+		
+		System.out.print("두번째 원의 중심과 반지름 입력 >> ");
+		int circle2X = scanner.nextInt();
+		int circle2Y = scanner.nextInt();
+		int circle2Radius=scanner.nextInt();
+		
+		int distanceSquare = (circle1X-circle2X)*(circle1X-circle2X)+(circle1Y-circle2Y)*(circle1Y-circle2Y);
+		int sumRadius = circle1Radius + circle2Radius;
+		if(distanceSquare<sumRadius)
+		{
+			System.out.println("두 원은 서로 겹친다.");
+		}
+		else
+		{
+			System.out.println("두 원은 서로 겹치지 않는다.");
+		}
+		
+		scanner.close();
+
+	}
+
+}
